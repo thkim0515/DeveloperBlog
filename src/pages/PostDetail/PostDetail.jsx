2443c4d9d0de347b47a74f185cea0da4f3c3388d
nano0912 <nano0912d@gmail.com>
2024-03-31 18:48:01 +0900

    rename & design : 페이지 컴포넌트단위로 폴더 수정, eiditor ui 추가

---
+++ src/pages/PostDetail/PostDetail.jsx
@@ -0,0 +1,8 @@
+import { PostDetailComponent } from '../../components/imagegallery/ImageGalleryComponents/PostDetailComponent';
+export const PostDetail = () => {
+  return (
+    <div>
+      <PostDetailComponent />
+    </div>
+  );
+};
