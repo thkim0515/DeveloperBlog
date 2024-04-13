019e34c805c06537efafc7bcaaa58175fcb357aa
nano0912 <nano0912d@gmail.com>
2024-04-13 22:30:11 +0900

    style : NotFound 페이지 코드 수정

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,20 +1,28 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import * as S from "./NotFound.style";
 
-export default function NotFound() {
+const NotFound = () => {
+  const navigate = useNavigate(); // useNavigate 훅을 함수 컴포넌트 내에서 사용
+
+  const goBack = () => {
+    navigate(-1); // 페이지를 이전으로 이동
+  };
+
   return (
-    <S.NotFoundLayout>
+    <S.NotFoundBox>
       <Helmet>
         <title>NotFound - StarBlog</title>
       </Helmet>
       <S.NotFoundWrapper>
         <S.Title>존재하지 않는 페이지 입니다.</S.Title>
-
         <S.Links>
-          <Link to="/"> 홈으로 이동 </Link>
+          {/* onClick으로 이벤트 처리기 설정 */}
+          <button onClick={goBack}>이전 페이지로 이동</button>
         </S.Links>
       </S.NotFoundWrapper>
-    </S.NotFoundLayout>
+    </S.NotFoundBox>
   );
-}
+};
+
+export default NotFound;
