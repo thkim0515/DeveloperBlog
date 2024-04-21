40b6d2f56a40dba95421e1e2bb8ae8b66f2ed6f4
thkim0515 <thkim0515@naver.com>
2024-04-21 15:56:29 +0900

    test

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const http = require("http");
