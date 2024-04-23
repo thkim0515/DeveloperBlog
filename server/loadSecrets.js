afaea7f27a194a80d5021113e7d313d28e326d10
thkim0515 <thkim0515@naver.com>
2024-04-23 09:23:22 +0900

    test

--- server/loadSecrets.js
+++ server/loadSecrets.js
@@ -6,8 +6,13 @@ const secretName = "starblog";
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
+const a = process.env.REACT_APP_A;
+const b = process.env.REACT_APP_B;
+
 AWS.config.update({
   region: region,
+  accessKeyId: a,
+  secretAccessKey: b,
 });
 const client = new AWS.SecretsManager();
 
