217ddb9c119d8028263e8574ba1d1bb0daaae698
thkim0515 <thkim0515@naver.com>
2024-12-09 17:29:59 +0900

    fix:backend deploy 조건문 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,22 @@ cd $FRONTEND_REPOSITORY
 sudo chown -R ubuntu:ubuntu $FRONTEND_REPOSITORY
 sudo chmod -R 755 $FRONTEND_REPOSITORY
 
-# nginx 재시작
+# Nginx 재시작
 sudo systemctl restart nginx
 
 # 백엔드 배포
 cd $BACKEND_REPOSITORY
 sudo chown -R ubuntu:ubuntu $BACKEND_REPOSITORY
 sudo chmod -R 755 $BACKEND_REPOSITORY
-pm2 restart all 
 
+# PM2 프로세스 관리
+if pm2 list | grep -q "backend"; then
+  echo "Restarting PM2 process..."
+  pm2 restart backend
+else
+  echo "Starting new PM2 process..."
+  pm2 start index.js --name backend
+fi
+
+# PM2 상태 저장
+pm2 save
